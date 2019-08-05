a7d8a494b1d6e1d8b6d4a3613e9f086209d1b5b3
Gustavo <Gustavo@g.coms>
2019-08-05 01:15:38 -0400

    First commit before storage link

---
+++ app/Product.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    protected $fillable = [
+        'name', 'description', 'image', 'price','type',
+    ];
+
+
+    public function getPriceAttribute($value){
+
+        $newForm = $value." Bs";
+        return $newForm;
+    
+    
+    }
+    
+
+}
